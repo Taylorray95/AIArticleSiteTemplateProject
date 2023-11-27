59523f22765f33e98d961834437cad3d3722c250
Taylor <Taylorleavelle@gmail.com>
2023-11-26 23:41:39 -0600

    Nearly complete with admin screensc - except for user administration. Still need to also work on roles/authorization

--- AIArticleSiteTemplateProject/AIArticleSiteTemplateProject/Objects/Category.cs
+++ AIArticleSiteTemplateProject/AIArticleSiteTemplateProject/Objects/Category.cs
@@ -3,6 +3,8 @@
     public class Category
     {
         public int CategoryId { get; set; }
+
+        public string? CategoryName { get; set; }
         public string? CategoryDesc { get; set; }
         public string? CategoryHashTag { get; set; }
         public DateTime SysDate { get; set; } = DateTime.Now;
