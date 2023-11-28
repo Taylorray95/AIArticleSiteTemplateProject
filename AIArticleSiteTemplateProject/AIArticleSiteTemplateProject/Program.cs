a12f23368d4b364913c2264804f9469a5e86cf55
Taylor <Taylorleavelle@gmail.com>
2023-11-27 23:24:52 -0600

    Finished category/post admin pages, fixed issue with LoginDisplay components styling/ error with logging out, added roles/assignment of 'SuperAdmin' role in the program.cs file.

--- AIArticleSiteTemplateProject/AIArticleSiteTemplateProject/Program.cs
+++ AIArticleSiteTemplateProject/AIArticleSiteTemplateProject/Program.cs
@@ -41,6 +41,7 @@ builder.Services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
@@ -79,5 +80,109 @@ app.MapRazorComponents<App>()
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    await EnsureRolesAsync(roleManager);
+    await AssignSuperUserRoleAsync(userManager);
+}
+
+async Task EnsureRolesAsync(RoleManager<IdentityRole> roleManager)
+{
+    string[] roles = new[] { "SuperAdmin", "Admin", "Moderator" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            var newRole = new IdentityRole(role);
+            await roleManager.CreateAsync(newRole);
+        }
+    }
+}
+async Task AssignSuperUserRoleAsync(UserManager<ApplicationUser> userManager)
+{
+    //once you have created your account, enter your email below and rerun the application to get your superuserrole which will give you access to do everything in the app 
+    //function can handle a list of users
+    var superUserEmails = new List<string>
+    {
+        "taylorleavelle@gmail.com"
+    };
+
+    foreach (var superUserEmail in superUserEmails)
+    {
+        try
+        {
+            var user = await userManager.FindByEmailAsync(superUserEmail);
+            if (user == null)
+            {
+                Console.WriteLine($"User with email {superUserEmail} not found.");
+                continue;
+            }
+
+            if (!await userManager.IsInRoleAsync(user, "SuperAdmin"))
+            {
+                var result = await userManager.AddToRoleAsync(user, "SuperAdmin");
+                if (result.Succeeded)
+                {
+                    Console.WriteLine($"User {superUserEmail} assigned to role successfully.");
+                }
+                else
+                {
+                    Console.WriteLine($"Error assigning role to {superUserEmail}: " + string.Join(", ", result.Errors.Select(e => e.Description)));
+                }
+            }
+            else
+            {
+                Console.WriteLine($"User {superUserEmail} already has the role.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception occurred while processing {superUserEmail}: " + ex.Message);
+        }
+    }
+}
+
+//async Task AssignSuperUserRoleAsync(UserManager<ApplicationUser> userManager)
+//{
+    
+//    var superUserEmail = "taylorleavelle@gmail.com";
+
+//    try
+//    {
+//        var user = await userManager.FindByEmailAsync(superUserEmail);
+//        if (user == null)
+//        {
+//            Console.WriteLine("User not found.");
+//            return;
+//        }
+
+//        if (!await userManager.IsInRoleAsync(user, "SuperAdmin"))
+//        {
+//            var result = await userManager.AddToRoleAsync(user, "SuperAdmin");
+//            if (result.Succeeded)
+//            {
+//                Console.WriteLine("User assigned to role successfully.");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Error assigning role: " + string.Join(", ", result.Errors.Select(e => e.Description)));
+//            }
+//        }
+//        else
+//        {
+//            Console.WriteLine("User already has the role.");
+//        }
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine("Exception occurred: " + ex.Message);
+//    }
+//}
+
 
 app.Run();
