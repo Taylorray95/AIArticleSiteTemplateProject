6688dc27100936bde25850623e31b26055b0c209
Taylor <Taylorleavelle@gmail.com>
2023-12-19 17:22:01 -0600

    fixed some styling issues

--- AIArticleSiteTemplateProject/AIArticleSiteTemplateProject/Program.cs
+++ AIArticleSiteTemplateProject/AIArticleSiteTemplateProject/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
-
+builder.Services.AddScoped<PostService>();
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultScheme = IdentityConstants.ApplicationScheme;
